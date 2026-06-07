b393f687d60a06318679cfa00cad0107535e6950
Kelsey-Hansel <kelseylhansel@gmail.com>
2026-06-06 19:46:25 -0500

    Updated code to try and fix processing error for routes.

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title : "Zoom Zoom",
         description: "An API that gives users access to 4 different vehicle types."
     },
-    host: "cse-341-final-project-proposal-zoomzoom.onrender.com",
+    host: "cse-341-final-project-proposal-zoomzoom-iy13.onrender.com",
     schemes: ["https"]
 }
 
